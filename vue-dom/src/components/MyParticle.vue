9e259e90dca160493470f274c61bed2b51b4c848
ics-ikeda <ikeda@ics-web.jp>
2019-07-28 14:51:41 +0900

    update Vue 2.6.10

---
+++ vue-dom/src/components/MyParticle.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="particle"
+       v-if="particle"
+       v-bind:style="{top: particle.y + 'px', left: particle.x + 'px'}">
+    😊
+  </div>
+</template>
+
+<script>
+  import ParticleData from "../ParticleData";
+
+  export default {
+    name: "MyParticle",
+    props: {
+      particle: ParticleData
+    }
+  };
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .particle {
+    position: absolute;
+  }
+</style>
